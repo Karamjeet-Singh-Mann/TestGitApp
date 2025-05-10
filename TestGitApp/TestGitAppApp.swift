60626c57f8fde53f5f117b7827949738cc888512
Karamjeet-Singh-Mann <singhkaramjeet250@gmail.com>
2025-05-10 08:51:19 +0530

    Initial Commit

---
+++ TestGitApp/TestGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitAppApp.swift
+//  TestGitApp
+//
+//  Created by karamjeet singh on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
